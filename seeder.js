e15f53685cc767ea16498fc7189ce45207b5818e
qassimjomart <62750225+qassimjomart@users.noreply.github.com>
2025-08-12 19:08:41 +0300

    Update seeder.js
    add banners

--- seeder.js
+++ seeder.js
@@ -4,27 +4,59 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const products = require('./data/products.js');
 const Product = require('./models/Product.js');
+const Banner = require('./models/bannerModel.js'); 
+const banners = require('./data/banners.js'); 
+
 
 dotenv.config();
 
+
 const importData = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected for seeding...');
-
-    // Очищаем коллекцию перед загрузкой
+    // Очистка старых данных
     await Product.deleteMany();
+    await User.deleteMany();
+    await Banner.deleteMany(); // <-- ДОБАВЬТЕ ОЧИСТКУ БАННЕРОВ
 
-    // Вставляем все товары из нашего файла
+    // Загрузка пользователей
+    await User.insertMany(users);
+    
+    // Загрузка товаров
     await Product.insertMany(products);
 
-    console.log('Data Imported Successfully!');
+    // --- 2. ДОБАВЬТЕ ЗАГРУЗКУ БАННЕРОВ ---
+    await Banner.insertMany(banners); // <-- ДОБАВЬТЕ ЭТУ СТРОКУ
+
+    console.log('Данные успешно импортированы (Пользователи, Товары, Баннеры)!'.green.inverse);
     process.exit();
   } catch (error) {
-    console.error(`Error with data import: ${error}`);
+    console.error(`${error}`.red.inverse);
     process.exit(1);
   }
 };
+
+const destroyData = async () => {
+  try {
+    await Product.deleteMany();
+    await User.deleteMany();
+    await Banner.deleteMany(); // <-- ДОБАВЬТЕ ОЧИСТКУ БАННЕРОВ И ЗДЕСЬ
+
+    console.log('Данные успешно удалены!'.red.inverse);
+    process.exit();
+  } catch (error) {
+    console.error(`${error}`.red.inverse);
+    process.exit(1);
+  }
+};
+
+// Логика запуска скрипта
+if (process.argv[2] === '-d') {
+  destroyData();
+} else {
+  importData();
+}
+
+
 
 // Запускаем функцию импорта
 importData();
