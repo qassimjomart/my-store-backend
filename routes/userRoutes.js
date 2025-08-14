62f815247a2fc074570345669ef4a27fb24a8448
qassimjomart <62750225+qassimjomart@users.noreply.github.com>
2025-08-14 19:57:14 +0300

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,12 +5,23 @@ const router = express.Router();
 const { 
     loginUser,
     registerUser,
-    getUserProfile 
+    getUserProfile,
+    updateUserProfile,
+    changeUserPassword
 } = require('../controllers/userController');
 const { protect } = require('../middleware/authMiddleware');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', protect, getUserProfile); // Этот маршрут защищен
+
+
+// Роут для обновления данных профиля (имя, email)
+// GET для получения профиля у вас, вероятно, уже есть. Добавляем PUT.
+router.route('/profile').put(protect, updateUserProfile); // .get(protect, getUserProfile)
+
+// Роут для смены пароля
+router.route('/profile/password').put(protect, changeUserPassword);
+
 
 module.exports = router;
